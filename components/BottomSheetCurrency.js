45b9f5c7e81972573d8dd7aba3496da099cc8601
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-11-05 16:21:02 +0530

    2 bottom sheets in add transaction

--- components/BottomSheetCurrency.js
+++ components/BottomSheetCurrency.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {List} from 'react-native-paper';
 
-const BottomSheetCurrency = () => {
+const BottomSheetCurrency = ({item, onPress}) => {
+  const handlePress = () => {
+    onPress(item.code);
+  };
+  console.log(item);
   return (
-    <View>
-      <Text>Currency</Text>
-    </View>
+    <List.Item
+      title={item.country}
+      description={item.symbol}
+      onPress={handlePress}
+      left={props => (
+        <List.Icon {...props} icon={{uri: item.flag}} color={item.color} />
+      )}
+    />
   );
 };
 
--- screens/home/AddTransactionScreen.js
+++ screens/home/AddTransactionScreen.js
@@ -5,15 +5,13 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import {
   Button,
   Caption,
-  Menu,
   TextInput,
-  List,
   useTheme,
+  Subheading,
 } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import BottomSheet, {BottomSheetFlatList} from '@gorhom/bottom-sheet';
-
-import ImageIcon from '../../components/ImageIcon';
 
 import BottomSheetCategory from '../../components/BottomSheetCategory';
 import BottomSheetCurrency from '../../components/BottomSheetCurrency';
@@ -38,8 +36,8 @@ const AddTransactionScreen = ({navigation}) => {
   const [selectedCurrency, setSelectedCurrency] = useState(
     avalibleExchangeRates.find(item => item.code === currency.base),
   );
-  const [bsType, setBsType] = useState('category');
-  const bottomSheetRef = useRef(null);
+  const categoryBottomSheetRef = useRef(null);
+  const currencyBottomSheetRef = useRef(null);
   const snapPoints = useMemo(() => [500], []);
 
   const handleDateChange = ({nativeEvent, type}) => {
@@ -56,21 +54,20 @@ const AddTransactionScreen = ({navigation}) => {
   const handleChangeCurrency = code => {
     const item = avalibleExchangeRates.find(currency => code === currency.code);
     setSelectedCurrency(item);
+    currencyBottomSheetRef.current.close();
   };
   const handleChangeCategory = category => {
     setSelectedCategory(category);
-    bottomSheetRef.current.close();
+    categoryBottomSheetRef.current.close();
   };
   const openBottomSheet = type => {
-    setBsType(type);
-    bottomSheetRef.current.snapToIndex(0);
-  };
-
-  const handleBsRender = ({item}) => {
-    if (bsType == 'category') {
-      return <BottomSheetCategory item={item} onPress={handleChangeCategory} />;
-    } else
-      return <BottomSheetCurrency item={item} onPress={handleChangeCurrency} />;
+    if (type === 'category') {
+      currencyBottomSheetRef.current.close();
+      categoryBottomSheetRef.current.snapToIndex(0);
+    } else {
+      categoryBottomSheetRef.current.close();
+      currencyBottomSheetRef.current.snapToIndex(0);
+    }
   };
 
   const handleSubmit = async () => {
@@ -96,19 +93,6 @@ const AddTransactionScreen = ({navigation}) => {
   };
   return (
     <View style={styles.screen}>
-      <View>
-        <Button onPress={() => setShowDate(true)}>Date</Button>
-        {showDate && (
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={transactionDate}
-            mode="date"
-            is24Hour={true}
-            display="default"
-            onChange={handleDateChange}
-          />
-        )}
-      </View>
       <View style={styles.addAmountContainer}>
         <TextInput
           placeholder="Transaction Amount"
@@ -122,14 +106,30 @@ const AddTransactionScreen = ({navigation}) => {
           {selectedCurrency.code}
         </Button>
       </View>
-      {selectedCurrency.code != currency.base && transactionAmount.length > 0 && (
-        <Caption>
-          {currency.symbol}
-          {((1 / selectedCurrency.rate) * parseInt(transactionAmount)).toFixed(
-            2,
+      <View>
+        {selectedCurrency.code != currency.base &&
+          transactionAmount.length > 0 && (
+            <Caption>
+              {currency.symbol}
+              {(
+                (1 / selectedCurrency.rate) *
+                parseInt(transactionAmount)
+              ).toFixed(2)}
+            </Caption>
           )}
-        </Caption>
-      )}
+        <Button onPress={() => setShowDate(true)}>Date</Button>
+        {showDate && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={transactionDate}
+            mode="date"
+            is24Hour={true}
+            display="default"
+            onChange={handleDateChange}
+          />
+        )}
+      </View>
+
       <TextInput
         placeholder="Note"
         label="Note"
@@ -138,14 +138,14 @@ const AddTransactionScreen = ({navigation}) => {
         onChangeText={text => setTransactionNote(text)}
       />
       <View style={styles.selectContainer}>
-        <Caption category="c1">Category</Caption>
+        <TranslateText category="subheading">Category</TranslateText>
         <TouchableNativeFeedback onPress={() => openBottomSheet('category')}>
           <View style={[styles.select, styles.padding]}>
             <View style={styles.select}>
-              <ImageIcon uri={selectedCategory.imageUri} />
-              <Caption category="h6">{selectedCategory.category}</Caption>
+              <Icon name="buffer" color={selectedCategory.color} size={30} />
+              <Subheading>{selectedCategory.category}</Subheading>
             </View>
-            <Caption category="h6">{selectedCategory.type}</Caption>
+            <Caption>{selectedCategory.type}</Caption>
           </View>
         </TouchableNativeFeedback>
       </View>
@@ -153,7 +153,7 @@ const AddTransactionScreen = ({navigation}) => {
         Add Transaction
       </Button>
       <BottomSheet
-        ref={bottomSheetRef}
+        ref={categoryBottomSheetRef}
         index={-1}
         snapPoints={snapPoints}
         backgroundStyle={[
@@ -166,10 +166,40 @@ const AddTransactionScreen = ({navigation}) => {
         handleIndicatorStyle={{backgroundColor: colors.accent}}
         enablePanDownToClose={true}>
         <BottomSheetFlatList
-          data={bsType === 'category' ? categories : avalibleExchangeRates}
+          data={categories}
           keyExtractor={item => item.category}
           contentContainerStyle={styles.contentContainer}
-          renderItem={item => handleBsRender(item)}
+          renderItem={item => (
+            <BottomSheetCategory
+              item={item.item}
+              onPress={handleChangeCategory}
+            />
+          )}
+        />
+      </BottomSheet>
+      <BottomSheet
+        ref={currencyBottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        backgroundStyle={[
+          styles.bottomsheet,
+          {
+            backgroundColor: colors.background,
+            borderColor: colors.primary,
+          },
+        ]}
+        handleIndicatorStyle={{backgroundColor: colors.accent}}
+        enablePanDownToClose={true}>
+        <BottomSheetFlatList
+          data={avalibleExchangeRates}
+          keyExtractor={item => item.category}
+          contentContainerStyle={styles.contentContainer}
+          renderItem={item => (
+            <BottomSheetCurrency
+              item={item.item}
+              onPress={handleChangeCurrency}
+            />
+          )}
         />
       </BottomSheet>
     </View>
@@ -183,19 +213,10 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 15,
   },
-  radioGroup: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
   addAmountContainer: {
     flexDirection: 'row',
     marginVertical: 15,
     justifyContent: 'center',
-  },
-  symbolSelect: {
-    width: '30%',
-    maxWidth: 150,
-    minWidth: 110,
   },
   seperator: {
     width: 15,
@@ -225,22 +246,3 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
 });
-
-{
-  /* <Menu
-          visible={changeCurrencyVisible}
-          anchor={
-            <Button
-              onPress={() => setChangeCurrencyVisible(!changeCurrencyVisible)}>
-              {selectedCurrency.code}
-            </Button>
-          }>
-          {avalibleExchangeRates.map(rate => (
-            <Menu.Item
-              key={rate.code}
-              onPress={() => handleChangeCurrency(rate.code)}
-              title={`${rate.symbol} (${rate.code})`}
-            />
-          ))}
-        </Menu> */
-}
