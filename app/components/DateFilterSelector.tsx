6c3a9b8a0c3c4ccd1315287cf6ee1c3daf5d2942
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2022-11-21 01:54:14 +0530

    date filter selector update

--- app/components/DateFilterSelector.tsx
+++ app/components/DateFilterSelector.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, View} from "react-native";
-import React from "react";
+import React, {useMemo} from "react";
 import {useCustomTheme} from "../themes";
 
 import {TouchableRipple, Paragraph} from "react-native-paper";
@@ -12,27 +12,47 @@ type Props = {
 
 const DateFilterSelector = ({onPress}: Props) => {
   const {
-    theme: {colors},
+    theme: {colors, fonts},
   } = useCustomTheme();
 
   const {dateFilter} = useData();
+
+  const text = useMemo(() => {
+    switch (dateFilter.type) {
+      case "today":
+      case "yesterday":
+        return format(dateFilter.startDate, "dd MMMM, yy");
+      case "thisWeek":
+      case "lastWeek":
+      case "thisMonth":
+      case "lastMonth":
+      case "thisYear":
+      case "last3Months":
+      case "last6Months":
+        return `${format(dateFilter.startDate, "dd MMM, yy")} - ${format(
+          dateFilter.endDate,
+          "dd MMM, yy",
+        )}`;
+      case "all":
+      default:
+        return "All Time";
+    }
+  }, [dateFilter]);
 
   return (
     <TouchableRipple
       style={{backgroundColor: colors.surface}}
       onPress={onPress}
     >
-      <View>
-        <Paragraph>{dateFilter.type}</Paragraph>
-        <Paragraph>
-          {format(dateFilter.startDate, "dd MMM yyyy: H:m:s")}
-        </Paragraph>
-        <Paragraph>{format(dateFilter.endDate, "dd MMM yyyy H:m:s")}</Paragraph>
-      </View>
+      <Paragraph style={[styles.align, fonts.medium]}>{text}</Paragraph>
     </TouchableRipple>
   );
 };
 
 export default DateFilterSelector;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  align: {
+    textAlign: "center",
+  },
+});
